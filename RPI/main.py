052404051f135ca3538a8e7880b9ab8870f6d726
Etka Kocak <ek223zf@student.lnu.se>
2025-05-12 15:30:19 +0200

    RPI circuit main

---
+++ RPI/main.py
@@ -0,0 +1,59 @@
+from machine import Pin, time_pulse_us
+import time
+
+TRIG_PIN = 27
+ECHO_PIN = 16
+trig = Pin(TRIG_PIN, Pin.OUT)
+echo = Pin(ECHO_PIN, Pin.IN)
+
+# Function to measure distance
+def measure_distance():
+    trig.low()
+    time.sleep_us(2)
+    trig.high()
+    time.sleep_us(10)
+    trig.low()
+    
+    duration = time_pulse_us(echo, 1)
+    
+    # Calculate the distance in cm
+    distance = (duration / 2) / 29.1
+    return distance
+
+# Check if the distance sensor is operating
+def check_distance_sensor(distance):
+    if distance < 0:
+        return False # Distance sensor not working
+    else:
+        return True
+
+threshold=150
+# Radar start
+print("Radar initialized. Waiting for vehicle....")
+while True:
+    distance = measure_distance()
+    # print("Distance: ", distance, "cm")
+    while distance < threshold and check_distance_sensor(distance):
+        start_distance = distance
+        start_time = time.ticks_ms()
+        while True:
+            end_distance = measure_distance()
+            # print("End Distance: ", end_distance, "cm")
+            if end_distance < 5: 
+                end_time = time.ticks_ms()
+                break
+            time.sleep(0.005)
+        delta_d = start_distance - end_distance  # cm
+        delta_t = time.ticks_diff(end_time, start_time) / 1000  # sec
+        if delta_t > 0:
+            speed = delta_d / delta_t  # cm/s actually
+            print("Speed:", round(speed, 2), "km/h")
+            time.sleep(0.005)
+            break 
+        else:
+            print("Calculation error.")
+            break
+
+    # print("Waiting for a new vehicle...")
+
+    time.sleep(0.01)
